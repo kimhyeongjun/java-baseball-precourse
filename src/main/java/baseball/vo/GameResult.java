80c67dbab0f6f1f9dc22cbf7eb44b74c2eb98a45
hyeongjun.kim <junkim0312@gmail.com>
2021-10-06 01:41:29 +0900

    bugfix : 게임 힌트 생성
    낫싱 힌트 생성 로직 추가

--- src/main/java/baseball/vo/GameResult.java
+++ src/main/java/baseball/vo/GameResult.java
@@ -28,23 +28,24 @@ public class GameResult {
 
   public String getHint() {
     StringBuilder resultMessage = new StringBuilder();
+    if (ballCount == 0 && strikeCount == 0) {
+      resultMessage.append("낫싱");
+    }
     if (strikeCount > 0) {
-      createHint(strikeCount, resultMessage);
-      resultMessage.append("스트라이크");
+      createHint(strikeCount, resultMessage, "스트라이크");
     }
     if (ballCount > 0) {
-      createHint(ballCount, resultMessage);
-      resultMessage.append("볼");
+      createHint(ballCount, resultMessage, "볼");
     }
     return resultMessage.toString();
   }
 
-  private void createHint(int count, StringBuilder message) {
+  private void createHint(int count, StringBuilder message, String hint) {
     if (message.length() > 0) {
       message.append(" ");
     }
 
-    message.append(count);
+    message.append(count).append(hint);
   }
 
   public static GameResultBuilder builder() {
