d75f63fd8ba22efbd7b3c5ef4bc66d7c7d7f27cd
hyeongjun.kim <junkim0312@gmail.com>
2021-10-05 17:45:09 +0900

    refactor : 입력된 데이터 검증
    생성자 접근 지정자 변경 private

--- src/main/java/baseball/util/Validator.java
+++ src/main/java/baseball/util/Validator.java
@@ -2,6 +2,8 @@ package baseball.util;
 
 public final class Validator {
 
+  private Validator() {}
+
   public static boolean checkInput(String numberStr) {
     return numberStr.matches("[0-9]+") && numberStr.length() == 3;
   }
