776a58759f84b80dbad8b9adba3b65440d164807
hyeongjun.kim <junkim0312@gmail.com>
2021-10-06 16:05:37 +0900

    test : 답 체크
    테스트 코드 수정

--- src/test/java/baseball/handler/BaseballGameTest.java
+++ src/test/java/baseball/handler/BaseballGameTest.java
@@ -1,12 +1,14 @@
 package baseball.handler;
 
 import baseball.vo.GameBoard;
+import nextstep.utils.Randoms;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.MockedStatic;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
@@ -16,6 +18,7 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class BaseballGameTest {
 
@@ -31,20 +34,19 @@ class BaseballGameTest {
   @ParameterizedTest
   @DisplayName("입력 된 데이터별 테스트")
   @MethodSource("generateData")
-  void doStart(int[] inputNums, int[] expectArr) throws NoSuchFieldException, IllegalAccessException {
-    Field field = baseballGame.getClass().getDeclaredField("baseBalls");
-    field.setAccessible(Boolean.TRUE);
+  void doCounting(int[] inputNums, int[] expectArr) {
+    try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+      mockRandoms
+        .when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+        .thenReturn(7, 8, 9);
+      baseballGame.init();
+      baseballGame.doStart(inputNums);
+      verify(expectArr);
+    }
+  }
 
-    List<Integer> randomNumbers = Arrays.asList(7, 8, 9);
-
-    field.set(baseballGame, randomNumbers);
-
-    baseballGame.doStart(inputNums);
-    GameBoard expectResult =
-      GameBoard.builder()
-        .strikeCount(expectArr[0])
-        .ballCount(expectArr[1])
-        .build();
+  private void verify(int[] expectArr) {
+    GameBoard expectResult = GameBoard.builder().strikeCount(expectArr[0]).ballCount(expectArr[1]).build();
 
     assertAll(
       () -> assertEquals(expectResult.getStrikeCount(), gameBoard.getStrikeCount()),
